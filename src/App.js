8f724225159a27da365e1e072d61d7700ec8cd9e
Dewansh Yadav <dewanshyadaw8@gmail.com>
2022-10-05 14:22:08 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './Components/Banner';
+import Header from './Components/Header';
+
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import About from './Components/About';
+import Team from './Components/Team';
+import Gallery from './Components/Gallery';
+import Contact from './Components/Contact';
+import Join from './Components/Join';
+import Footer from './Components/Footer';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ "overflow-x": "hidden"}}>
+
+
+      <Router>
+
+      <Header/>
+
+
+      <Routes>
+   
+       <Route exact path='/' element={<Banner/>}  ></Route>
+       <Route exact path='/about' element={<About/>}  ></Route>
+       <Route exact path='/team' element={<Team/>}  ></Route>
+       <Route exact path='/gallery' element={<Gallery />}  ></Route>
+       <Route exact path='/join' element={<Join/>}  ></Route>
+       <Route exact path='/contact' element={<Contact />}  ></Route>
+
+
+
+      </Routes>
+
+      <Footer/>
+ 
+
+
+       </Router>
+
     </div>
+  
+
+  
   );
 }
 
